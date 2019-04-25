81adb80c7dc278c5c0c74686bb209a35297215b1
Toni Sefton <21288605+Ariadne-S@users.noreply.github.com>
2019-04-25 13:22:47 +1000

    Querying Temperature Forecast via Willy Weather API

---
+++ IggieNotifications/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+using Newtonsoft.Json;
+
+namespace IggieNotifications
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (HttpClient client = new HttpClient()) {
+                // Call asynchronous network methods in a try/catch block to handle exceptions
+                try {
+                    HttpResponseMessage response = await client.GetAsync("https://api.willyweather.com.au/v2/{ApiKey}/locations/8190/weather.json?forecasts=temperature&days=2&startDate=2019-04-25");
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    var forcastResponse = JsonConvert.DeserializeObject<WillyWeatherForecastTempResponse>(responseBody);
+
+                    foreach (var day in forcastResponse.Forecasts.Temperature.Days) {
+                        foreach (var entry in day.Entries) {
+                            var entryDateTime = entry.DateTime.ToString("f");
+                            var entryTemperature = entry.Temperature.ToString();
+                            Console.WriteLine("The temperature at {0} is {1} celsius", entryDateTime, entryTemperature);
+                        }
+                    }
+
+                } catch (HttpRequestException e) {
+                    Console.WriteLine("\nException Caught!");
+                    Console.WriteLine("Message :{0} ", e.Message);
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
---
+++ IggieNotifications/WillyWeatherForecastTempResponse.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IggieNotifications
+{
+    public class WillyWeatherForecastTempResponse
+    {
+
+        public Location Location { get; set; }
+        public Forecasts Forecasts { get; set; }
+    }
+
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Region { get; set; }
+        public string State { get; set; }
+        public string Postcode { get; set; }
+        public string TimeZone { get; set; }
+    }
+
+    public class Forecasts
+    {
+        public TemperatureResults Temperature { get; set; }
+    }
+
+    public class TemperatureResults
+    {
+        public List<DaysResults> Days { get; set; }
+        public UnitResults Units { get; set; }
+    }
+
+    public class DaysResults
+    {
+        public DateTime DateTime { get; set; }
+        public List<EntryResults> Entries { get; set; }
+    }
+
+    public class EntryResults
+    {
+        public DateTime DateTime { get; set; }
+        public decimal Temperature { get; set; }
+    }
+
+    public class UnitResults
+    {
+        public string Temperature { get; set; }
+    }
+}
